f06f65f3df051ec1a01b7ec16d60a03700601bf4
unknown <www.khushalnandaniya@gmail.com>
2024-01-06 13:51:08 +0530

    update about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import "./about.css";
 import Me from "../../about.jpg";
-import {FaAward} from "react-icons/fa";
-import {HiUsers} from "react-icons/hi";
-import {MdLocationPin} from "react-icons/md";
+import { FaAward } from "react-icons/fa";
+import { HiUsers } from "react-icons/hi";
+import { MdLocationPin } from "react-icons/md";
 
 const About = () => {
   return (
@@ -11,37 +11,37 @@ const About = () => {
       <h5>Get To Know</h5>
       <h2>About Me</h2>
 
-    <div className='container about_container'>
-      <div className='about_me'>
-      <div className='about_img'>
-        <img src={Me} alt='about-img'/>
-      </div>
-      </div>
-      <div className='about_content'>
-        <div className='about_cards'>
-          <article className='about_card'>
-            <FaAward className='about_icon'/>
-            <h5>Eduction</h5>
-            <small>4th Year Computer Engineer Student</small>
-          </article>
-          <article className='about_card'>
-            <HiUsers className='about_icon'/>
-            <h5>Grades</h5>
-            <small>7.67 CGPA</small>
-          </article>
-          <article className='about_card'>
-            <MdLocationPin className='about_icon'/>
-            <h5>Loaction</h5>
-            <small>Mehsana</small>
-          </article>
+      <div className='container about_container'>
+        <div className='about_me'>
+          <div className='about_img'>
+            <img src={Me} alt='about-img' />
+          </div>
         </div>
+        <div className='about_content'>
+          <div className='about_cards'>
+            <article className='about_card'>
+              <FaAward className='about_icon' />
+              <h5>Eduction</h5>
+              <small>Final Year Computer Engineer Student</small>
+            </article>
+            <article className='about_card'>
+              <HiUsers className='about_icon' />
+              <h5>Grades</h5>
+              <small>7.67 CGPA</small>
+            </article>
+            <article className='about_card'>
+              <MdLocationPin className='about_icon' />
+              <h5>Loaction</h5>
+              <small>Ahmedabad</small>
+            </article>
+          </div>
 
-        <p>"Hello! I'm Khushal Nandaniya, a passionate and motivated 4th-year computer engineering student specializing in both frontend and backend development. With a strong foundation in computer science and programming, I possess a solid understanding of web technologies and frameworks.
- </p>
+          <p>"Hello! I'm Khushal Nandaniya, a passionate and motivated 4th-year computer engineering student specializing in both frontend and backend development. With a strong foundation in computer science and programming, I possess a solid understanding of web technologies and frameworks.
+          </p>
 
-        <a href='#contact' className='btn btn-primary'>Let's talk</a>
+          <a href='#contact' className='btn btn-primary'>Let's talk</a>
         </div>
-    </div>
+      </div>
     </section>
   )
 }
